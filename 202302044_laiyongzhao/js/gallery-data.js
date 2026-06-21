8fb432d6ef62fce7a055ca328d1afa1cd9612c9c
yongzhaolai99-max <yongzhaolai99@gmail.com>
2026-06-21 22:14:09 +0900

    Delete 202302044_laiyongzhao directory

--- 202302044_laiyongzhao/js/gallery-data.js
+++
@@ -1,8 +0,0 @@
-const galleryData = [
-{id:1,title:"구름 벽",category:"battle",categoryName:"Sky",date:"Cloud Wall",image:"images/gallery/photo01.jpg",desc:"운벽은 대기 역학이 강제로 따뜻하고 습한 기류를 상승시키면서, 수증기가 냉각되어 응결하여 구름을 형성하고, 경계가 뚜렷하여 '벽' 모양을 이루는 것이다."},
-{id:2,title:"봄",category:"battle",categoryName:"Season",date:"Spring",image:"images/gallery/photo02.jpg",desc:"봄은 만물이 소생하고 생기가 넘치는 계절이다."},
-{id:3,title:"여름",category:"adventure",categoryName:"Season",date:"Summer",image:"images/gallery/photo03.jpg",desc:"여름은 열정이 넘치고 활력이 가득한 계절이다."},
-{id:4,title:"흐림",category:"friendship",categoryName:"Sky",date:"Cloudy",image:"images/gallery/photo04.jpg",desc:"열대의 하늘은 계절에 상관없이 항상 그렇다"},
-{id:5,title:"가을",category:"special",categoryName:"Season",date:"Autumn",image:"images/gallery/photo05.jpg",desc:"가을은 수확과 침전의 계절이다."},
-{id:6,title:"겨울",category:"special",categoryName:"Season",date:"Winter",image:"images/gallery/photo06.jpg",desc:"겨울은 고요함과 힘을 쌓는 계절이다."}
-];
